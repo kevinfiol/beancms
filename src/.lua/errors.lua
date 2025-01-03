344a2807acc704ee5dc1b266cc91c774b0dc2f83
kevinfiol <me@kevinfiol.com>
2025-01-03 14:42:17 -0500

    deleting this for now

--- src/.lua/errors.lua
+++
@@ -1,8 +0,0 @@
-local constant = require 'constants'
-
-return {
-  [constant.USER_DOES_NOT_EXIST] = 'User does not exist',
-  [constant.USER_EXISTS] = 'User already exists, try another username',
-  [constant.PASSWORD_MISMATCH] = 'Passwords must match',
-  [constant.WRONG_PASSWORD] = 'Invalid Password'
-}
